f70adee7a546c17446be59e626b3bba294ffb4fe
Sheila R Moore <sherexmykes@github.com>
2020-02-12 20:14:23 -0500

    get user

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.send("You have arrived");
 });
+
+
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+    if (newUser.name && newUser.bio) {
+        db.insert(newUser)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({
+            errorMessage:
+                "There was an error while saving the user to the database"
+            });
+        });
+    } else {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+    }
+});
