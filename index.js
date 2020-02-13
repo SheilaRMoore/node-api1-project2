f02f33fa8684c4eff3a15a248b45b1aba89d1c51
Sheila R Moore <sherexmykes@github.com>
2020-02-12 20:17:52 -0500

    get indvidual user, delete

--- index.js
+++ index.js
@@ -29,3 +29,71 @@ server.post('/api/users', (req, res) => {
         .json({ errorMessage: "Please provide name and bio for the user." });
     }
 });
+
+server.get('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    db.findById(userId)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist."
+                });
+            }
+
+        })
+        .catch(err => {
+            res.status(404).json({
+                errorMessage: "The user information could not be retrieved."
+            });
+        });
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    db.remove(userId)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({
+                  message: "The user with the specified ID does not exist."
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+              errorMessage: "The user could not be removed"
+            });
+        })
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const editUser = req.body;
+  if (editUser.name && editUser.bio) {
+    db.update(editUser.id, editUser)
+      .then(user => {
+        if (user) {
+          res.status(201).json(user);
+        } else {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          errorMessage: "The user information could not be modified."
+        });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
