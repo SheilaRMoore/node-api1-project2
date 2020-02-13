3b68ae8aeaa345671f3577413e61ac0ffbd1aeb2
Sheila R Moore <sherexmykes@github.com>
2020-02-12 20:09:30 -0500

    get request

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express = require('express');
+const port = 5000;
+const db = require('./data/db.js');
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("You have arrived");
+});
